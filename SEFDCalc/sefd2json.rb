b9555e1250267019f6cefee3ea069bb0a457ffae
Jon Richards <jrseti@gmail.com>
2018-07-21 23:41:54 +0000

    New version

--- SEFDCalc/sefd2json.rb
+++ SEFDCalc/sefd2json.rb
@@ -11,14 +11,16 @@ sources = Hash.new();
 
 filename = ARGV[0];
 File.open(filename).each do |line|
-	#taua,5c,y,4000.00,0.000000,0.000000
+	#taua,5c,y,4000.00,0.000000,0.000000,fname
+	if(line.include?("skip")) then next; end
 	parts = line.split(",");
 	source = parts[0];
 	ant = parts[1];
 	pol = parts[2];
 	freq = parts[3];
 	sefd = parts[4];
-	ratio = parts[5].chomp;
+	ratio = parts[5];
+	fname = parts[6].chomp;
 
 	ants[ant] = 1;
 	sources[source] = 1;
@@ -32,8 +34,39 @@ File.open(filename).each do |line|
 	if(h[source][ant][pol] == nil)
 		h[source][ant][pol] = [];
 	end
-	h[source][ant][pol] << [freq, sefd];
+	h[source][ant][pol] << [freq, sefd, fname];
 end
+
+=begin
+filename = ARGV[1];
+File.open(filename).each do |line|
+	#casa,4g,y,9000.00,39008.822899,67.708553,4gy_9000.00_casa_292.png
+	line.chomp!;
+	if(line.start_with?("skip")) then next; end
+	parts = line.split(",");
+	if(parts.length != 7) then next; end
+	source = parts[0];
+	ant = parts[1];
+	pol = parts[2];
+	freq = parts[3];
+	sefdx = parts[4];
+	sefdy = parts[5];
+	png = parts[6];
+
+	if(h[source] == nil || h[source][ant] == nil) then next; end
+
+
+	if(h[source][ant][pol] != nil)
+		h[source][ant][pol].each do |fs|
+			if(fs[0].to_f == freq.to_f)
+				fs << png;
+			end
+		end
+	end
+end
+=end
+
+
 
 antlist = [];
 ants.each do |key, value|
