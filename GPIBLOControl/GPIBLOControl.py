2db257dbacd6ca4f4aee9120eb87f2685e2dd628
kulpaj <kulpaj.dev@gmail.com>
2019-08-13 14:40:30 -0400

    fixing minor GPIBLO program issues and adding an extended manual

--- GPIBLOControl/GPIBLOControl.py
+++ GPIBLOControl/GPIBLOControl.py
@@ -7,12 +7,13 @@ import sys
 if __name__ == "__main__":
     usage = """Usage %prog [options] command [commandopt]
     command selection:
-        setfreq  freq - set the frequency
+        setfreq  freq - set the frequency [Hz]
         errors - list all errors
-        init - resets the device do default settings
+        init - resets the device do default settings and turn it on
         on - turns the power on
         off - turns the power off
-        getfreq - returns the current frequency"""
+        getfreq - returns the current frequency
+        getoutputstate - returns True/False based on output state (On/Off)"""
     parser = OptionParser(usage=usage)
     parser.add_option("-a", "--gpibaddres", action="store", type="int", dest="address", default=30,
                       help="GPIB address od the device, default=%default", metavar="ADDR")
@@ -20,8 +21,10 @@ if __name__ == "__main__":
                       help="hostname of the controller default=%default", metavar="HOST")
     parser.add_option("-w", "--wait", action="store", type="int", dest="wait", default=True,
                       help="wait for *OPC? to finish, default=%default", metavar="BOOL")
-    parser.add_option("-v", "--verbose", action="store", type="int", dest="v", default=True,
-                      help="prints more, default=%default", metavar="BOOL")
+    parser.add_option("-v", "--verbose", action="store_true", dest="v", default=True,
+                      help="print more output, default=%default")
+    parser.add_option("-q", "--quiet",action="store_false", dest="v",
+                      help="print less output")
     
     (options, args) = parser.parse_args()
     
@@ -30,7 +33,7 @@ if __name__ == "__main__":
         parser.print_help()
         sys.exit(1)
         
-    validopts = ['setfreq','errors','init','on','off','getfreq']
+    validopts = ['setfreq','errors','init','on','off','getfreq','getoutputstate']
     
     if any(args[0] in s for s in validopts):
         if args[0] == 'setfreq' and len(args) < 2:
@@ -73,6 +76,7 @@ if __name__ == "__main__":
             print "initializing device"
         commandmodule.reset_device()
         commandmodule.set_defaults()
+        commandmodule.output_on(options.wait)
         if options.v:
             setF = commandmodule.get_frequency()
             print "current frequency is " + str(setF/1e9) + " GHz"
@@ -89,7 +93,10 @@ if __name__ == "__main__":
     
     if args[0] == 'getfreq':
         setF = commandmodule.get_frequency()
-        print "current frequency is " + str(setF/1e9) + " GHz"
+        print "current frequency is " + str(setF) + " Hz"
+        
+    if args[0] == 'getoutputstate':
+        print commandmodule.is_transmitting()
         
     if options.v:
         isT = commandmodule.is_transmitting()
--- GPIBLOControl/README.md
+++ GPIBLOControl/README.md
@@ -1,4 +1,38 @@
 # GPIB-LO-Control
 Python program to control the local oscilator using the GPIB to Ethernet adapter
 Tested with HP 83731B
-main program is called GPIBLOControl, whereas control package is an internal library
+
+the repository consist of library (control.py) and a shell program (GPIBLOControl.py)
+
+The library allows to send and receive any arbitrary SCPI command, but also 
+have some predefined functions. The default power level is +13 dBm and it is not
+possible to control it in the program (it is defined in the library)
+
+The shell program has predefined command options
+
+Usage: Usage GPIBLOControl.py [options] command [commandopt]
+    command selection:
+        setfreq  freq - set the frequency [Hz]
+        errors - list all errors
+        init - resets the device do default settings
+        on - turns the power on
+        off - turns the power off
+        getfreq - returns the current frequency [Hz]
+        getoutputstate - returns True/False based on output state (On/Off)
+
+Options:
+  -h, --help            show this help message and exit
+  -a ADDR, --gpibaddres=ADDR
+                        GPIB address od the device, default=30
+  -s HOST, --host=HOST  hostname of the controller default=lo2.hcro.org
+  -w BOOL, --wait=BOOL  wait for *OPC? to finish, default=True
+  -v, --verbose         print more output, default=True
+  -q, --quiet           print less output
+
+examples:
+setting LO to default settings and turning it on:
+    GPIBLOControl.py -q init
+setting LO frequency to 2 GHz (note that HP 83731B works in frequency range 1-20 GHz)
+    GPIBLOControl.py -q setfreq 2e9
+checking all errors in the query and state of the device
+    GPIBLOControl.py -v errors
