3891a56dcf6075b7c18b552b6c0c53f9fd1ba316
Janusz S. Kulpa (@nsg-work) <kulpaj.dev@gmail.com>
2020-01-22 04:41:37 +0000

    minor bugfix

--- pythonLibs/SNAPobs/snap_observations.py
+++ pythonLibs/SNAPobs/snap_observations.py
@@ -7,13 +7,13 @@ main functions for snap observations
 
 from ATATools import logger_defaults,obs_db,ata_control
 import snap_defaults
-import snap_recorder
 
-def observe_same(ant_dict,freq,source,ncaptures,obstype,obsuser,desc,filefragment,az_offset=0,el_offset=0,fpga_file=spectra_snap_file,obs_set_id=None):
+def observe_same(ant_dict,freq,source,ncaptures,obstype,obsuser,desc,filefragment,rms=None,
+        az_offset=0,el_offset=0,fpga_file=snap_defaults.spectra_snap_file,obs_set_id=None):
     
 
     import pdb
-    pbd.set_trace()
+    pdb.set_trace()
 
 
 
