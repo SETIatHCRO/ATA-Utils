e747d3e6bb9a7f7b85324efc28029e9937aa2251
HCRO Observer <obs@hcro.org>
2020-08-06 00:55:01 +0000

    fixing calls for python3

--- PamMeas/process_data.py
+++ PamMeas/process_data.py
@@ -174,44 +174,60 @@ def makePolynomial(ar,doPolyTest=0):
 
     return pp,rest        
 
+def makeDictionary(pb,data,polys,rest,isok,pol,sigtype):
+    retdict = {'pbstr' : pb, 'pol': pol, 'type': sigtype, 'ok': bool(isok)}
+    for a in numpy.arange(21):
+        retdict['m' + str(a)] = float(data[1,a]);
+        retdict['d' + str(a)] = float(data[0,a]);
+    for a in numpy.arange(6):
+        retdict['p' + str(a)] = float(polys[-a-1]);
+    retdict['vallow'] = float(rest[0])
+    retdict['valhigh'] = float(rest[1])
+    return retdict
+
 def genDatabaseQuery(pb,data,polys,rest,isok,database_name, update_flag):
     
     #check if db exist?
-    dict1 = {'pbstr' : pb, 'pol': 'x', 'type': 'cw', 'ok': isok[0]}
-    for a in numpy.arange(21):
-        dict1['m' + str(a)] = data[keyXCW][1,a];
-        dict1['d' + str(a)] = data[keyXCW][0,a];
-    for a in numpy.arange(6):
-        dict1['p' + str(a)] = polys[keyXCW][-a-1];
-    dict1['vallow'] = rest[keyXCW][0]
-    dict1['valhigh'] = rest[keyXCW][1]
-        
-    dict2 = {'pbstr' : pb, 'pol': 'x', 'type': 'n', 'ok': isok[0]}
-    for a in numpy.arange(21):
-        dict2['m' + str(a)] = data[keyXNoise][1,a];
-        dict2['d' + str(a)] = data[keyXNoise][0,a];
-    for a in numpy.arange(6):
-        dict2['p' + str(a)] = polys[keyXNoise][-a-1];
-    dict2['vallow'] = rest[keyXNoise][0]
-    dict2['valhigh'] = rest[keyXNoise][1]
+    #dict1 = {'pbstr' : pb, 'pol': 'x', 'type': 'cw', 'ok': bool(isok[0])}
+    #for a in numpy.arange(21):
+    #    dict1['m' + str(a)] = data[keyXCW][1,a];
+    #    dict1['d' + str(a)] = data[keyXCW][0,a];
+    #for a in numpy.arange(6):
+    #    dict1['p' + str(a)] = polys[keyXCW][-a-1];
+    #dict1['vallow'] = rest[keyXCW][0]
+    #dict1['valhigh'] = rest[keyXCW][1]
+
+    dict1 = makeDictionary(pb,data[keyXCW],polys[keyXCW],rest[keyXCW],isok[0],'x','cw')
+
+    #dict2 = {'pbstr' : pb, 'pol': 'x', 'type': 'n', 'ok': bool(isok[0])}
+    #for a in numpy.arange(21):
+    #    dict2['m' + str(a)] = data[keyXNoise][1,a];
+    #    dict2['d' + str(a)] = data[keyXNoise][0,a];
+    #for a in numpy.arange(6):
+    #    dict2['p' + str(a)] = polys[keyXNoise][-a-1];
+    #dict2['vallow'] = rest[keyXNoise][0]
+    #dict2['valhigh'] = rest[keyXNoise][1]
+    dict2 = makeDictionary(pb,data[keyXNoise],polys[keyXNoise],rest[keyXNoise],isok[0],'x','n')
     
-    dict3 = {'pbstr' : pb, 'pol': 'y', 'type': 'cw', 'ok': isok[1]}
-    for a in numpy.arange(21):
-        dict3['m' + str(a)] = data[keyYCW][1,a];
-        dict3['d' + str(a)] = data[keyYCW][0,a];
-    for a in numpy.arange(6):
-        dict3['p' + str(a)] = polys[keyYCW][-a-1];
-    dict3['vallow'] = rest[keyYCW][0]
-    dict3['valhigh'] = rest[keyYCW][1]
+    #dict3 = {'pbstr' : pb, 'pol': 'y', 'type': 'cw', 'ok': bool(isok[1])}
+    #for a in numpy.arange(21):
+    #    dict3['m' + str(a)] = data[keyYCW][1,a];
+    #    dict3['d' + str(a)] = data[keyYCW][0,a];
+    #for a in numpy.arange(6):
+    #    dict3['p' + str(a)] = polys[keyYCW][-a-1];
+    #dict3['vallow'] = rest[keyYCW][0]
+    #dict3['valhigh'] = rest[keyYCW][1]
+    dict3 = makeDictionary(pb,data[keyYCW],polys[keyYCW],rest[keyYCW],isok[1],'y','cw')
         
-    dict4 = {'pbstr' : pb, 'pol': 'y', 'type': 'n', 'ok': isok[1]}
-    for a in numpy.arange(21):
-        dict4['m' + str(a)] = data[keyYNoise][1,a];
-        dict4['d' + str(a)] = data[keyYNoise][0,a];
-    for a in numpy.arange(6):
-        dict4['p' + str(a)] = polys[keyYNoise][-a-1];
-    dict4['vallow'] = rest[keyYNoise][0]
-    dict4['valhigh'] = rest[keyYNoise][1]
+    #dict4 = {'pbstr' : pb, 'pol': 'y', 'type': 'n', 'ok': bool(isok[1])}
+    #for a in numpy.arange(21):
+    #    dict4['m' + str(a)] = data[keyYNoise][1,a];
+    #    dict4['d' + str(a)] = data[keyYNoise][0,a];
+    #for a in numpy.arange(6):
+    #    dict4['p' + str(a)] = polys[keyYNoise][-a-1];
+    #dict4['vallow'] = rest[keyYNoise][0]
+    #dict4['valhigh'] = rest[keyYNoise][1]
+    dict2 = makeDictionary(pb,data[keyYNoise],polys[keyYNoise],rest[keyYNoise],isok[1],'y','n')
     
     if database_name != "none":
         if database_name == "google":
