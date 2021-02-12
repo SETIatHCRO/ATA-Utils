2718eb5caf1d137107c6a4fdf8cbb335935ce38b
wfarah <wael.a.farah@gmail.com>
2021-02-12 01:53:11 +0000

    parameters for frb-node observing

--- pythonLibs/SNAPobs/snap_dada/snap_dada_defaults.py
+++ pythonLibs/SNAPobs/snap_dada/snap_dada_defaults.py
@@ -4,8 +4,10 @@ udpdb_script = "udpdb.sh"
 dbsumdb_script = "ata_dbsumdb"
 dbsigproc_script = "dbsigproc.sh"
 dbnull_script = "dbnull.sh"
-NIC_cores = [8,9,10,11,12,13,14,15,24,25,26,27,28,29,30,31]
-proc_cores = [4,5,6,7,16,17,18,19,20,21,22,23]
+#NIC_cores = [8,9,10,11,12,13,14,15,24,25,26,27,28,29,30,31]
+NIC_cores = [8,9,10,11,12,13,14,15]
+#proc_cores = [4,5,6,7,16,17,18,19,20,21,22,23]
+proc_cores = [4,5,6,7]
 #bufsze = 16777216 # for spectrometer data, nbit=32 npol=4 nchan=4096
 #bufsze = 134217728 # for spectrometer data, nbit=32 npol=4 nchan=4096, nsamp=2048
 bufsze = 67108864 # for spectrometer data, nbit=32 npol=4 nchan=4096, nsamp=1024
