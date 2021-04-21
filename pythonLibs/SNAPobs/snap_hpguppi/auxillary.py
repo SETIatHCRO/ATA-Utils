2d2e17e1237de5cfcda840434067a48b96360fc7
RocketRoss <code@radonn.co.za>
2021-04-21 09:20:05 +0000

    ++ snap_hpguppi.auxillary redis channel generation

--- pythonLibs/SNAPobs/snap_hpguppi/auxillary.py
+++ pythonLibs/SNAPobs/snap_hpguppi/auxillary.py
@@ -1,5 +1,6 @@
-from SNAPobs import snap_config
+from SNAPobs import snap_config, snap_dada
 from . import snap_hpguppi_defaults as hpguppi_defaults
+from . import record_in as hpguppi_record_in
 import re
 
 # Gather antenna-configuration for the listed snaps
@@ -23,3 +24,25 @@ def get_snap_hostname_dict_for_antenna_names(antenna_names):
 def redis_get_channel_from_set_channel(set_channel):
   match = re.match(hpguppi_defaults.REDISSETGW_re, set_channel)
   return hpguppi_defaults.REDISGETGW.substitute(host=match.group('host'), inst=match.group('inst'))
+
+def redis_set_channel_from_get_channel(get_channel):
+  match = re.match(hpguppi_defaults.REDISGETGW_re, get_channel)
+  return hpguppi_defaults.REDISSETGW.substitute(host=match.group('host'), inst=match.group('inst'))
+
+def _generate_hpguppi_redis_channels(hpguppi_hostnames, hpguppi_instance_ids, redisgw_template):
+  return [redisgw_template.substitute(host=hostname, inst=instid) 
+          for hostname in hpguppi_hostnames for instid in hpguppi_instance_ids]
+
+def generate_hpguppi_redis_set_channels(hpguppi_hostnames, hpguppi_instance_ids):
+  return _generate_hpguppi_redis_channels(hpguppi_hostnames, hpguppi_instance_ids, hpguppi_defaults.REDISSETGW)
+
+def generate_hpguppi_redis_get_channels(hpguppi_hostnames, hpguppi_instance_ids):
+  return _generate_hpguppi_redis_channels(hpguppi_hostnames, hpguppi_instance_ids, hpguppi_defaults.REDISGETGW)
+
+def generate_freq_auto_string_per_channel(redis_obj, hpguppi_redis_get_channels):
+  log_string_per_channel = []
+  for channel in hpguppi_redis_get_channels:
+    snaps = hpguppi_record_in._get_snaps_of_redis_chan(redis_obj, channel)
+    antdict = get_antenna_name_dict_for_snap_hostnames(snaps)
+    log_string_per_channel.append(str(snap_dada.get_freq_auto([antdict[snap] for snap in snaps])))
+  return log_string_per_channel
