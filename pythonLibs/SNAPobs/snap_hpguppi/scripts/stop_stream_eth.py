6b49364671a7c33af057c5944f680ee8b86ccfd7
RocketRoss <code@radonn.co.za>
2021-08-10 09:08:54 +0000

    ++ SNAPobs/hpguppi/scripts/stop_stream -H - regex empowered hostname specification

--- pythonLibs/SNAPobs/snap_hpguppi/scripts/stop_stream_eth.py
+++ pythonLibs/SNAPobs/snap_hpguppi/scripts/stop_stream_eth.py
@@ -4,6 +4,7 @@ Script to stop the ethernet output of input snap hosts
 """
 from SNAPobs import snap_control, snap_config
 from SNAPobs.snap_hpguppi import auxillary as hpguppi_auxillary
+import re
 import time
 import argparse
 import sys
@@ -30,10 +31,21 @@ if __name__ == '__main__':
                         help='The list of Antenna-LO streams to stop')
     parser.add_argument('-S', '--enable-eth', action='store_true',
                         help='Enable the ethernet output instead of stopping it.')
+    parser.add_argument('-H', '--hostnames', nargs='*', type=str,
+                    help='The DSP source stream hostnames as comma (,) separated lists [] (hostnames can be regex, overrules groupings)',
+                    default=[])
     parser.add_argument('--all', action='store_true',
                         help='Stop the ethernet output of every stream (listed in ATA_SNAP_TAB) before configuring...')
     args = parser.parse_args()
-    
+
+    if len(args.hostnames) > 0:
+        ATA_SNAP_TAB = snap_config.get_ata_snap_tab()
+        args.antlo_stream_list = []
+        for hostname in args.hostnames:
+            for hostname_criterion in hostname.split(','):
+                hostname_pattern = re.compile(hostname_criterion)
+                args.antlo_stream_list += [row['antlo'] for idx,row in ATA_SNAP_TAB.iterrows() if hostname_pattern.match(row['snap_hostname'])]
+
     feng_objs = collect_feng_obj(args.antlo_stream_list, args.all, True)
     print('Affects the following F-Engine streams:\n', [feng.host for feng in feng_objs])
 
