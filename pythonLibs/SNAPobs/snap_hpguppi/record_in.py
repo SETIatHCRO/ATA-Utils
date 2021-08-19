1abddba16c1c0c0460fd7922a8425ba46e987632
RocketRoss <code@radonn.co.za>
2021-08-19 07:22:22 +0000

    ** SNAPobs/hpguppi/record_in: overhaul - gather sync_times but _calculate_obs_start_stop once

--- pythonLibs/SNAPobs/snap_hpguppi/record_in.py
+++ pythonLibs/SNAPobs/snap_hpguppi/record_in.py
@@ -18,7 +18,7 @@ from . import auxillary as hpguppi_auxillary
 DEFAULT_START_IN = 2
 DEFAULT_OBS_TIME = 300.
 
-def _log_recording(start_time, duration, obsstart, npackets, redisset, extra_string):
+def _log_recording(start_time, duration, obsstart, npackets, redisset_chan_list, chan_log_strings):
     logdir = '~'
     if os.path.exists('/home/sonata/logs'):
         logdir = '/home/sonata/logs'
@@ -26,15 +26,16 @@ def _log_recording(start_time, duration, obsstart, npackets, redisset, extra_str
     with open(os.path.join(logdir, 'record_in_log.csv'), 'a', newline='') as csvfile:
         csvwr = csv.writer(csvfile, delimiter=',',
                             quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        row_strings = [
-            str(datetime.fromtimestamp(start_time)),
-            str(duration),
-            str(obsstart),
-            str(npackets),
-            str(redisset),
-            str(extra_string)
-        ]
-        csvwr.writerow(row_strings)
+        for i, redisset_chan in enumerate(redisset_chan_list):
+            row_strings = [
+                str(datetime.fromtimestamp(start_time)),
+                str(duration),
+                str(obsstart),
+                str(npackets),
+                str(redisset_chan),
+                str(chan_log_strings[i]) if chan_log_strings is not None else ''
+            ]
+            csvwr.writerow(row_strings)
 
 def _get_sync_time_for_snaps(snaps):
     fengs = snap_control.init_snaps(snaps)
@@ -69,13 +70,18 @@ def block_until_hpguppi_idling(hashes, verbose=True):
 def block_until_post_processing_waiting(hashes, verbose=True):
     _block_until_key_has_value(hashes, "PPSTATUS", "WAITING", verbose=verbose)
 
-def _publish_obs_start_stop(redis_obj, channel, obsstart, obsstop, dry_run=False):
+def _publish_obs_start_stop(redis_obj, channel_list, obsstart, obsstop, dry_run=False):
     cmd = "OBSSTART=%i\nOBSSTOP=%i\n"  %(obsstart, obsstop)
     cmd += "PKTSTART=%i\nPKTSTOP=%i"  %(obsstart, obsstop)
-    print(channel, cmd.replace('\n', '\t'))
-    if not dry_run:
-        redis_obj.publish(channel, cmd)
-    else:
+
+    if isinstance(channel_list, str):
+        channel_list = [channel_list]
+    
+    for channel in channel_list:
+        print(channel, cmd.replace('\n', '\t'))
+        if not dry_run:
+            redis_obj.publish(channel, cmd)
+    if dry_run:
         print('***DRY RUN***')
 
 def _calculate_obs_start_stop(t_start, duration_s, sync_time, tbin):
@@ -114,36 +120,35 @@ def record_in(
         print("Will broadcast the OBSSTART and OBSSTOP values, based on redishost's SYNCTIME of", universal_sync_time)
         print()
     
-    for channel_i, channel in enumerate(hpguppi_redis_set_channels):
-        assert re.match(hpguppi_defaults.REDISSETGW_re, channel) or channel == hpguppi_defaults.REDISSET
-        additional_log_string = log_string_per_channel[channel_i] if log_string_per_channel is not None else None
+    if not reset:
+        sync_times = []
+        for channel_i, channel in enumerate(hpguppi_redis_set_channels):
+            assert re.match(hpguppi_defaults.REDISSETGW_re, channel) or channel == hpguppi_defaults.REDISSET
 
-        sync_time = universal_sync_time
-        if not reset and not force_synctime:
-            snaps = hpguppi_auxillary.get_snaps_of_redis_chan(hpguppi_defaults.redis_obj, hpguppi_auxillary.redis_get_channel_from_set_channel(channel))
-            # print(snaps)
-            sync_times = _get_sync_time_for_snaps(snaps)
-            if len(set(sync_times)) == 1:
-                sync_time = sync_times[0]
-            else:
-                print("Hpguppi channel", channel, "has the following snaps, with non-uniform sync-times:")
-                for i in range(len(snaps)):
-                    print(snaps[i], sync_times[i])
-                print("Cannot reliably start a recording.")
-                return False
+            if not reset and not force_synctime:
+                snaps = hpguppi_auxillary.get_snaps_of_redis_chan(hpguppi_defaults.redis_obj, hpguppi_auxillary.redis_get_channel_from_set_channel(channel))
+
+                channel_sync_times = _get_sync_time_for_snaps(snaps)
+                sync_times.extend(channel_sync_times)
+                if len(set(sync_times)) != 1:
+                    print("Hpguppi channel", channel, "has the following snaps, with non-uniform sync-times:")
+                    for i in range(len(snaps)):
+                        print(snaps[i], channel_sync_times[i])
+                    print("Cannot reliably start a recording.")
+                    return False
         
-        if not reset:
-            obsstart, obsstop, npackets = _calculate_obs_start_stop(t_in_x, obs_duration_s, sync_time, hpguppi_defaults.TBIN)
-        
-        _publish_obs_start_stop(hpguppi_defaults.redis_obj, channel, obsstart, obsstop, dry_run)
-        if log and not reset:# and not dry_run:
-            _log_recording(
-                t_in_x,
-                obs_duration_s,
-                obsstart,
-                npackets,
-                channel,
-                additional_log_string
-                )
+        sync_time = universal_sync_time if force_synctime else sync_times[0]
+        obsstart, obsstop, npackets = _calculate_obs_start_stop(t_in_x, obs_duration_s, sync_time, hpguppi_defaults.TBIN)
+    
+    _publish_obs_start_stop(hpguppi_defaults.redis_obj, hpguppi_redis_set_channels, obsstart, obsstop, dry_run)
+    if log and not reset:# and not dry_run:
+        _log_recording(
+            t_in_x,
+            obs_duration_s,
+            obsstart,
+            npackets,
+            hpguppi_redis_set_channels,
+            log_string_per_channel
+            )
 
     return True
