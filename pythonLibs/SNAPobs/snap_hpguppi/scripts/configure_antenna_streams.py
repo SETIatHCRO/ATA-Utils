2f2509a057f9e2bf8630bb017304b814c639b5ef
RocketRoss <code@radonn.co.za>
2021-11-11 16:29:35 +0000

    * SNAPobs/hpguppi/config_ant_strms -C, ++ - ++ -C group-config-stem - + sort_rfsoc_config - + --no-blank

--- pythonLibs/SNAPobs/snap_hpguppi/scripts/configure_antenna_streams.py
+++ pythonLibs/SNAPobs/snap_hpguppi/scripts/configure_antenna_streams.py
@@ -13,10 +13,24 @@ import casperfpga
 import redis
 import socket
 import re
+import yaml
 
 import sync_streams
 
 import os, sys
+
+def sort_rfsoc_config(rfsoc_config):
+	pipeline_ids = rfsoc_config['pipeline_ids'].copy()
+	pipeline_ids.sort()
+	original_indices = [rfsoc_config['pipeline_ids'].index(pipeline_id) for pipeline_id in pipeline_ids]
+	
+	rfsoc_config_sorted = rfsoc_config.copy()
+	rfsoc_config_sorted['pipeline_ids'] = pipeline_ids
+	rfsoc_config_sorted['feng_ids'] = [rfsoc_config['feng_ids'][index] for index in original_indices]
+	if rfsoc_config['dests'] is not None:
+		rfsoc_config_sorted['dests'] = [rfsoc_config['dests'][index] for index in original_indices]
+	return rfsoc_config_sorted
+	
 
 # sys.path.insert(0, '/home/sonata/miniconda3/bin/')
 sys.path.insert(0, '/home/sonata/dev/ata_snap/sw/ata_snap/scripts/')
@@ -40,7 +54,7 @@ parser.add_argument('-s', '--stop-all-eth-first', action='store_true',
 										help='Stop the ethernet output of every snap (listed in ATA_SNAP_TAB) before configuring')
 parser.add_argument('-p', '--prog-snaps', action='store_true',
 										help='Program the snaps being configured')
-parser.add_argument('-C', '--skip-conf', action='store_true',
+parser.add_argument('-S', '--sync-only', action='store_true',
 										help='Skip configuring the snaps (will still sync them)')
 parser.add_argument('-g', '--groupings', nargs='+', type=str,
 										help='The sub-grouping of DSP source streams as comma (,) separated lists ["{}"]'.format('", "'.join(default_stream_subs)),
@@ -54,8 +68,14 @@ parser.add_argument('-d', '--config-directory', type=str,
 parser.add_argument('-c', '--config-stem', type=str,
 										help='The stem of the sub-grouping configuration yaml files (1 indexed) ["{}"]'.format(default_cfg_stem),
 										default=default_cfg_stem)
+parser.add_argument('-C', '--group-config-stem', type=str,
+										help='The stem of the collective configuration yaml file (1 for all groups) [{}]'.format(None),
+										default=None)
 parser.add_argument('-t', '--test-vector-enable', action='store_true',
 										help='Configure the antenna streams to send test vectors [False]',
+										default=False)
+parser.add_argument('--no-blank', action='store_true',
+										help='Do not blank out un-configured pipelines',
 										default=False)
 parser.add_argument('--dry-run', action='store_true',
 										help='Don\'t actually configure anything.')
@@ -80,18 +100,20 @@ stream_cfgs = []
 stream_subs = []
 for stream_id, stream_str in enumerate(args.groupings):
 	stream_subs.append(stream_str.split(','))
-	stream_cfgs.append(os.path.join(args.config_directory, '{}{}.yml'.format(args.config_stem, stream_id+1)))
-	if not os.path.exists(stream_cfgs[-1]):
-		stream_cfgs[-1] = os.path.join(args.config_directory, '{}.yml'.format(args.config_stem))
-	assert os.path.exists(stream_cfgs[-1]), '{} path doesn\'t exist'.format(stream_cfgs[-1])
+	stream_cfg = os.path.join(args.config_directory, '{}.yml'.format(args.group_config_stem))
+	if args.group_config_stem is None:
+		stream_cfg = os.path.join(args.config_directory, '{}{}.yml'.format(args.config_stem, stream_id+1))
+		if not os.path.exists(stream_cfg):
+			stream_cfg = os.path.join(args.config_directory, '{}.yml'.format(args.config_stem))
+	
+	assert os.path.exists(stream_cfg), '{} path doesn\'t exist'.format(stream_cfg)
+	stream_cfgs.append(stream_cfg)
 
 	print('Sub-array #{}: {}\t configured with {}'.format(stream_id, stream_subs[-1], stream_cfgs[-1]))
 
 stream_antlo_names = list(itertools.chain(*stream_subs)) # should be a list of antlo
 stream_hostnames = hpguppi_auxillary.get_stream_hostname_per_antenna_names(stream_antlo_names)
 stream_antlo_hostname_dict = hpguppi_auxillary.get_stream_hostname_dict_for_antenna_names(stream_antlo_names)
-
-fengs = snap_control.init_snaps(stream_hostnames)
 
 # Collect the redis channels of the hpguppi_daq instances that are
 # being streamed to by the antennae to be configured, as well as the
@@ -100,7 +122,7 @@ fengs = snap_control.init_snaps(stream_hostnames)
 ## as selective silencing of antennae, such that instances only receive
 ## packets from a whole configuration
 
-# TODO load IPs of hpguppi_daq instances from somewhere
+# TODO load IPs of hpguppi_daq instances from the collective destination-IP list
 ips = [
 	'10.11.1.51',
 	'10.11.1.61',
@@ -155,9 +177,15 @@ if not args.dry_run:
 else:
 	print('hpguppi_daq redis channels to reset:', hpguppi_redis_reset_chans)
 
-if not args.skip_conf:
+if not args.sync_only:
 	rfsoc_hostname_regex = r'(?P<boardname>.*)-(?P<pipeline>\d+)$'
 	rfsoc_hostname_configurations_dict = {}
+
+	multi_group_config = None
+	if args.group_config_stem is not None:
+		with open(stream_cfgs[0], 'r') as fh:
+			multi_group_config = yaml.load(fh, Loader=yaml.SafeLoader)
+			assert isinstance(multi_group_config['voltage_output']['dests'][0], list), 'The voltage_output dests is not a nested list, which is expected of multi-group configuration files.'
 
 	for (sub_id, stream_sub) in enumerate(stream_subs):
 		print('Configuring Subarray', sub_id)
@@ -167,6 +195,7 @@ if not args.skip_conf:
 			stream_hostname = stream_antlo_hostname_dict[stream_antlo_name]
 
 			if stream_hostname.startswith('frb-snap'):
+				assert multi_group_config is None, 'SNAP configuration has not been expanded to support a single multi-group configuration file.'
 				fpgfile = snap_config.get_ata_cfg()['SNAPFPG']
 								
 				print('{} Reprogramming/configuring snap as FEngine #{:02d} {}'.format('v'*5, feng_id, 'v'*5))
@@ -204,29 +233,40 @@ if not args.skip_conf:
 						'config_yml'	: stream_cfgs[sub_id],
 						'feng_ids'		: [feng_id],
 						'pipeline_ids': [rfsoc_pipeline],
-						# 'dest_port'		: [10000 for i in range(8)],
+						'dests'				: None,
 						'sync'				: True,
 						'eth_volt'		: True,
 						'tvg'					: args.test_vector_enable,
-						'noblank'			: False,
+						'noblank'			: args.no_blank,
 						'skip_prog'		: not args.prog_snaps
 					}
-					print()
+					if multi_group_config is not None:
+						rfsoc_hostname_configurations_dict[rfsoc_boardname]['dests']	= [multi_group_config['voltage_output']['dests'][sub_id]]
+							
 				else:
 					rfsoc_hostname_configurations_dict[rfsoc_boardname]['feng_ids'].append(feng_id)
 					rfsoc_hostname_configurations_dict[rfsoc_boardname]['pipeline_ids'].append(rfsoc_pipeline)
+					if multi_group_config is not None:
+						rfsoc_hostname_configurations_dict[rfsoc_boardname]['dests'].append(multi_group_config['voltage_output']['dests'][sub_id])
+				
 				print('{} Batched reprogramming/configuring RFSoC {} as FEngine #{:02d} {}'.format('='*5, stream_hostname, feng_id, '='*5))		
 			else:
 				print('Cannot make out the kind of FEngine board with hostname \'{}\''.format(stream_hostname))
 		print()
 		
+	
+	# Batch program the rfsocs
 	for rfsoc_boardname, rfsoc_config in rfsoc_hostname_configurations_dict.items():
 		print('{} Batched reprogramming/configuring RFSoC {} {}'.format('v'*5, rfsoc_boardname, 'v'*5))	
 		rfsoc_hostname = rfsoc_boardname + '-1'
-		print('rfsoc_feng_init.py {} {} {} -i {} -j {} {}{}{}{}'.format(
+		
+		print(rfsoc_config)
+		rfsoc_config = sort_rfsoc_config(rfsoc_config)
+		print('rfsoc_feng_init.py {} {} {} -i {} -j {} {}{}{}{}{}'.format(
 				rfsoc_hostname, rfsoc_config['fpga_file'], rfsoc_config['config_yml'],
 				' '.join(map(str, rfsoc_config['feng_ids'])),
 				' '.join(map(str, rfsoc_config['pipeline_ids'])),
+				'-d {} '.format(' '.join([','.join(dests) for dests in rfsoc_config['dests']])) if rfsoc_config['dests'] else '',
 				'-s ' if rfsoc_config['sync'] else '',
 				'--eth_volt ' if rfsoc_config['eth_volt'] else '',
 				'-t ' if rfsoc_config['tvg'] else '',
@@ -234,14 +274,15 @@ if not args.skip_conf:
 				'--skipprog' if rfsoc_config['skip_prog'] else ''
 				)
 			)
+		
 		if args.dry_run:
 			print('*'*5, 'Dry Run', '*'*5)
 		else:
 			rfsoc_feng_init.run(
 				rfsoc_hostname, rfsoc_config['fpga_file'], rfsoc_config['config_yml'],
 				feng_ids = rfsoc_config['feng_ids'],
-				# dest_port = rfsoc_config['dest_port'],
 				pipeline_ids = rfsoc_config['pipeline_ids'],
+				dests = rfsoc_config['dests'],
 				sync = rfsoc_config['sync'],
 				eth_volt = rfsoc_config['eth_volt'],
 				tvg = rfsoc_config['tvg'],
