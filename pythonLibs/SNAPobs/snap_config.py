a063f36d002a300c7953afd7a3c16f801e298266
sonata <wael.a.farah@gmail.com>
2022-11-07 19:13:39 +0000

    Add function to pull available RFSoC antennas

--- pythonLibs/SNAPobs/snap_config.py
+++ pythonLibs/SNAPobs/snap_config.py
@@ -1,5 +1,6 @@
 import os,sys
 import pandas as pd
+import toml
 
 from SNAPobs import snap_defaults
 from ATATools import ata_helpers
@@ -42,3 +43,29 @@ def get_ata_snap_if():
 
 def get_ata_base_obs_dir():
     return ATA_BASE_OBS_DIR
+
+def get_ata_obsinfo():
+    """
+    Return obsinfo.toml file as a python dictionary
+    """
+    f = open(os.path.join(ATA_SHARE_DIR, 'obsinfo.toml'), "r")
+    obsinfo = toml.load(f)
+    return obsinfo
+
+def get_rfsoc_active_antlist():
+    """
+    Return active ata antenna list from obsinfo.toml file
+    """
+    import numpy as np
+
+    obsinfo = get_ata_obsinfo()
+    input_map = np.array(obsinfo['input_map'])
+    x_pol_ants = input_map[np.where(input_map[:,1] == 'x')][:,0]
+    y_pol_ants = input_map[np.where(input_map[:,1] == 'y')][:,0]
+
+    if not (x_pol_ants == y_pol_ants).all():
+        import warnings
+        warnings.warn("xpol and ypol antennas seem to be different. "
+                "Will return xpol antennas anyway")
+
+    return x_pol_ants.tolist()
