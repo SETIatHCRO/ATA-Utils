3c277ffd92bbcf8f15e9661daac5a557238ae13d
Tusay <tusay@psu.edu>
2023-07-17 20:50:10 +0000

    fixed basic_tseti.sh

--- NbeamAnalysis/basic_tseti.sh
+++ NbeamAnalysis/basic_tseti.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
-# Check if a directory name is provided as an argument
-if [ $# -eq 0 ]
+# Takes 2 inputs, .fil file directory and an output directory.
+# Creates subfolders with .dat files in the output directory.
+
+# Check if both input and output directory names are provided as arguments
+if [ $# -ne 2 ]
   then
-    echo "No directory name provided. Usage: $0 <directory>"
+    echo "Usage: $0 <input directory> <base output directory>"
     exit 1
 fi
 
-# Store the directory name in a variable
-dir=$1
+# Store the input and base output directory names in variables
+indir=$1
+outdir=$2
 
-# Check if the directory exists
-if [ ! -d $dir ]
+if [[ ${outdir} != */ ]];then
+    outdir=$outdir/
+    echo $outdir
+fi
+
+# Check if the input directory exists
+if [ ! -d $indir ]
   then
-    echo "Directory $dir does not exist."
+    echo "Directory $indir does not exist."
     exit 1
 fi
 
 # Recursively search for '*.fil' files in the directory
-find $dir -type f -name '*.fil' -print0 | while read -d $'\0' fil; do
+find $indir -type f -name '*.fil' -print0 | while read -d $'\0' fil; do
     # Create the output directory with the same structure as the input directory
-    outdir=$(dirname "${fil/$dir/outdir}")
-    mkdir -p $outdir
-    
+    relpath=$(realpath --relative-to=$indir $(dirname $fil))
+    mkdir -p "$outdir$relpath"
+    # echo $outdir$relpath
+
     # Execute the turboSETI command for each '*.fil' file found
-    turboSETI $fil -s 10 -o $outdir -g y -l warning
+    turboSETI $fil -s 10 -o $outdir$relpath -g y -l warning
     
     # Remove the .h5 files from the output directory
-    rm -f $outdir/*.h5
+    rm -f $outdir$relpath/*.h5
 done
