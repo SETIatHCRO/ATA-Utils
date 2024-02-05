2744d72797d95ab35d7ab808722bf49ac30937ce
marcjacquart <60528499+marcjacquart@users.noreply.github.com>
2024-02-05 15:02:32 -0800

    simplify telnet parsing info putting everything in one variable instead of a loop on the lines.

--- WeatherStation/update_data.py
+++ WeatherStation/update_data.py
@@ -26,32 +26,40 @@ class TelnetLink():
         with readable variable names from 'var_name_dict'.
 
         Output:
-        parsed_values_dict: python dictionnary with parsed values
+        parsed_values: python dictionnary with parsed values
         '''
 
         self.tn.read_until(b"0r1,") # Remove what's before
-        line1 = self.tn.read_until(b"0r2,") #0r1 line
-        line2 = self.tn.read_until(b"0r5,") #0r2 line
-        line3 = self.tn.read_until(b"0r3,") #0r5 line
-        line4 = self.tn.read_until(b"0r1,") #0r3 line
+        telnet_data = self.tn.read_until(b"0r1,") # Read one block of telnet data
 
-        # Transform the telnet strings to a python 2d list for each line and each variable
 
-        # TODO: Put everything on one line to avoid all the 'for' loops
-        # Then don't read line by line: replace '\n' by ','
-        table_raw_values = [
-            line1.decode().split(','),
-            line2.decode().split(','),
-            line3.decode().split(','),
-            line4.decode().split(',')
-        ]
+        # Transform the telnet byte string to a python list
+        raw_values = telnet_data.decode().split(',')
 
         # Data to extract and length of WS data unit to remove.
-        vars_to_extract_tab = ( # format: (Name in telnet, charcter length of unit)
-            (('Dn=', 1), ('Dm=', 1), ('Dx=', 1), ('Sn=', 1), ('Sm=', 1), ('Sx=', 9)), #0r1 line
-            (('Ta=', 1), ('Ua=', 1), ('Pa=', 9)), # 0r2 line
-            (('Th=', 1), ('Vh=', 1), ('Vs=', 1), ('Vr=', 1),('Id=',-1)), # 0r5 line
-            (('Rc=', 1), ('Rd=', 1), ('Ri=', 1), ('Hc=', 1),('Hd=', 1), ('Hi=', 0)), # 0r3 line
+        # Format: (Name in telnet, charcter length of unit)
+        # See variable definition below in 'var_name_dict'
+        vars_to_parse_tab = (
+            ('Dn=', 1),
+            ('Dm=', 1),
+            ('Dx=', 1),
+            ('Sn=', 1),
+            ('Sm=', 1),
+            ('Sx=', 9),
+            ('Ta=', 1),
+            ('Ua=', 1),
+            ('Pa=', 9),
+            ('Th=', 1),
+            ('Vh=', 1),
+            ('Vs=', 1),
+            ('Vr=', 1),
+            ('Id=', 8),
+            ('Rc=', 1),
+            ('Rd=', 1),
+            ('Ri=', 1),
+            ('Hc=', 1),
+            ('Hd=', 1),
+            ('Hi=', 0),
         )
 
         # Transform to human readable variable names, def in Vaisala WXT530 user guide
@@ -78,28 +86,25 @@ class TelnetLink():
             'Hi': 'HailIntens', # Hail intensity [hits/cm^2 h]
         }
 
-        parsed_values_dict = {}
-        # Line by line (telnet output)
-        for idx_line, one_line_values in enumerate(vars_to_extract_tab):
-            pcouldnt_parse = '' # To display variables that failed to parse
-            current_raw_table_line = table_raw_values[idx_line] # One line of split telnet data
+        parsed_values = {}
+        couldnt_parse = '' # To display variables that failed to parse
 
-            for idx, var_to_extract in enumerate(one_line_values):
-                # Look if we find matching variable to extract
-                if current_raw_table_line[idx][:len(var_to_extract[0])] == var_to_extract[0]:
-                    translated_name = var_name_dict[var_to_extract[0][:-1]]
-                    var_name = f"{self.link_name}_{translated_name}"
-                    # Create result directory entery
-                    parsed_values_dict[var_name] = current_raw_table_line[idx][len(var_to_extract[0]):-var_to_extract[1]]
+        for idx, var_to_parse in enumerate(vars_to_parse_tab):
+            # Look if we find matching variable to extract
+            if raw_values[idx][:len(var_to_parse[0])] == var_to_parse[0]:
+                translated_name = var_name_dict[var_to_parse[0][:-1]]
+                var_name = f"{self.link_name}_{translated_name}"
+                # Create result directory entery
+                parsed_values[var_name] = raw_values[idx][len(var_to_parse[0]):-var_to_parse[1]]
 
-                else: # Couldn't find the WS variable in the table
-                    pcouldnt_parse += f'{var_to_extract[0][:-1]}, '
+            else: # Couldn't find the WS variable in the table
+                couldnt_parse += f'{var_to_parse[0][:-1]}, '
 
-            if pcouldnt_parse != '': # Parsing of at least one WS value failed
-                # TODO: Handle the error? (e.g.: reset_connection?)
-                print(f"Parsing failed for vaules: {pcouldnt_parse}")
+        if couldnt_parse != '': # Parsing of at least one WS value failed
+            # TODO: Handle the error? (e.g.: reset_connection?)
+            print(f"Parsing failed for vaules: {couldnt_parse}")
 
-        return parsed_values_dict
+        return parsed_values
 
 
     def reset_connection(self):
