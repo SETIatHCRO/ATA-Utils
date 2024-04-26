3ff8e87c8c3686b292490e5cac084ead789ad6f1
marcjacquart <60528499+marcjacquart@users.noreply.github.com>
2024-04-25 17:38:49 -0700

    Added atastatus window at the start, works on obs-node2

--- TerminatorObservationConsole/HCRO_OBS_launch.sh
+++ TerminatorObservationConsole/HCRO_OBS_launch.sh
@@ -5,8 +5,11 @@ gpg -d ~/TerminatorObservationConsole/.sonata.password.gpg >/dev/null || exit 1
 ssh-add -t 300
 nohup terminator --config ~/TerminatorObservationConsole/terminator_config_HCRO --layout HCROOBS --profile=HCRO </dev/null &>/dev/null &
 
+# Open atastatus page
+nohup ssh obs@control.hcro.org 'atastatus' &
+
 # Automatically open webpages relevant to HCRO observation
+xhost +local:
 nohup ~/TerminatorObservationConsole/scripts/webpages.sh
 
-# Open atastatus page
-nohup ~/TerminatorObservationConsole/scripts/atastatus.exp
+
