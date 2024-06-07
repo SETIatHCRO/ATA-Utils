e18726f1b5a75bc20232c2e2f7806cd7f3d11aee
sonata <wael.a.farah@gmail.com>
2024-06-07 19:16:09 +0000

    add 'atacheck' script

--- pythonLibs/setup.py
+++ pythonLibs/setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     name='ATA-Utils-pythonLibs',
     version=version.__version__,
     #install_requires=['ephem','astropy','numpy','tftpy','pyuvdata<=1.4;python_version<"3"','pyuvdata;python_version>"3.5"'],
-    install_requires=['ephem','astropy','numpy','tftpy', 'pandas', 'bottleneck','plotly','dash','requests', 'toml'],
+    install_requires=['ephem','astropy','numpy','tftpy', 'pandas', 'bottleneck','plotly','dash','requests', 'toml', 'pytz'],
     description='python utility scripts for ATA (private repo)',
     license='MIT',
     packages=['ATAdb', 'ATATools','ATAobs','SNAPobs','SNAPobs.snap_dada', 'SNAPobs.snap_hpguppi', 
@@ -20,10 +20,13 @@ setuptools.setup(
     keywords=['ATA'],
     long_description=long_description,
     url="https://github.com/SETIatHCRO/ATA-Utils/",
-    scripts=['SNAPmon/atasnapmon.py', 'SNAPobs/snap_hpguppi/scripts/meta_marshall.py',
-             'SNAPobs/snap_hpguppi/scripts/set_postproc_keys.py', 
-             'SNAPobs/snap_hpguppi/scripts/set_hashpipe_keys.py', 
-             'SNAPobs/snap_hpguppi/scripts/populate_meta.py', 'SNAPobs/snap_hpguppi/scripts/start_record_in_x.py', 
-             'ATAPointing/pointing_elxel_plot.py', 'ATAPointing/pointing_azel_table.py','ATAPointing/pointing_azel_print.py']
+    scripts=['SNAPmon/atasnapmon.py',
+        'SNAPobs/snap_hpguppi/scripts/meta_marshall.py',
+        'SNAPobs/snap_hpguppi/scripts/set_postproc_keys.py',
+        'SNAPobs/snap_hpguppi/scripts/set_hashpipe_keys.py',
+        'SNAPobs/snap_hpguppi/scripts/populate_meta.py', 'SNAPobs/snap_hpguppi/scripts/start_record_in_x.py',
+        'ATAPointing/pointing_elxel_plot.py',
+        'ATAPointing/pointing_azel_table.py',
+        'ATAPointing/pointing_azel_print.py']
     #entry_points={'console_scripts': ['GPIBLOControl = GPIBLOControl.GPIBLOControl:main']},
 )
